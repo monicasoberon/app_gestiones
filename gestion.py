06b802ab9af6c6194c5f999183bd227ef6bb0d60
Mónica Soberón Zubía <monicasoberon2747@gmail.com>
2024-09-20 10:12:12 -0600

    Update gestion.py

--- gestion.py
+++ gestion.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from snowflake.snowpark.functions import col
-from msal_streamlit_authentication import msal_authentication
+from streamlit_msal import Msal
 
 client_id = "674d8292-6dc4-4f8f-a4d0-575f1e0837c8"
 authority = "https://login.microsoftonline.com/876969de-3b40-4648-872a-0ebecb3489e6"
