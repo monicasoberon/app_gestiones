abcd47d697f9ef5fd78f4058aa408624896064ad
Monica Soberon Zubía <monica.soberon@lamosa.com>
2024-10-09 09:21:56 -0600

    diseño

--- app.py
+++ app.py
@@ -31,10 +31,8 @@ def authenticate():
                 st.image("Imagenes/LogoLamosa.png", width=300)
             with col2:
                 st.image("Imagenes/LogoOficial.png", width=300)
-
-            # Display the header and message below the images
-            st.header("Bienvenido a la página de Gestiones y Estadísticas de la Comunidad de Analítica.")
-            st.write("Por favor autenticate con tu correo de Lamosa.")
+            st.markdown("<h2 style='text-align: center;'>Bienvenido a la página de Gestiones y Estadísticas de la Comunidad de Analítica.</h2>", unsafe_allow_html=True)
+            st.markdown("<p style='text-align: center;'>Por favor autenticate con tu correo de Lamosa.</p>", unsafe_allow_html=True)
             st.stop()
 
     return st.session_state.get("auth_data")
